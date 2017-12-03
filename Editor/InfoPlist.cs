04a745643182a4f2682d16f490215235c591a01c
Adrian Akison <adrian@akison.com>
2017-12-04 08:22:40 +1000

    Start on a new editor script for managing info.plist

---
+++ Editor/InfoPlist.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using System.IO;
+using System.Collections.Generic;
+
+public class InfoPlist : MonoBehaviour {
+
+    // TODO: Move to SerializedObject?
+
+    public static bool usesCamera = true;
+
+    public static string cameraUsage = "Used to take photos of items in need of repair";
+
+#if UNITY_IOS
+    [PostProcessBuild]
+    public static void OnPostprocessBuild(BuildTarget buildTarget, string path) {
+        // Read plist
+        var plistPath = Path.Combine(path, "Info.plist");
+        var plist = new PlistDocument();
+        plist.ReadFromFile(plistPath);
+
+        // Update value
+        //if(usesCamera) {
+            PlistElementDict rootDict = plist.root;
+            rootDict.SetString("NSCameraUsageDescription", cameraUsage);
+        //}
+
+        // Write plist
+        File.WriteAllText(plistPath, plist.WriteToString());
+    }
+#endif
+}
