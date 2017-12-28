a8d5ba20b030c8a1f6526ba12a45964033b1574d
Adrian Akison <adrian@akison.com>
2017-12-28 11:30:52 +1000

    Add enum drawer

---
+++ Inspector/EnumFlagAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+namespace Relentless {
+
+    [SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Flag")]
+    [AttributeUsage(AttributeTargets.Enum)]
+    public sealed class EnumFlagAttribute : PropertyAttribute {
+
+        public EnumFlagAttribute() { }
+
+        public EnumFlagAttribute(string enumName) {
+            EnumName = enumName;
+        }
+
+        public string EnumName { get; private set; }
+
+    }
+
+}
